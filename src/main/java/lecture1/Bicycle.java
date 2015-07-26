b97d53f982b420b545d235ce6fc24a1a3972a922
MaksymD <maksym.dotsenko@gmail.com>
2015-07-26 21:01:34 +0200

    1

--- src/main/java/lecture1/Bicycle.java
+++ src/main/java/lecture1/Bicycle.java
@@ -1,12 +1,14 @@
-<<<<<<< HEAD
+import sun.plugin2.message.transport.Transport;
+
+//<<<<<<< HEAD
 package com.my.lecture1;
 =======
 package lecture1;
 >>>>>>> origin/master
 
-public class Bicycle implements Transport{
+public class Bicycle implements Transport {
 
     public void move() {
-        System.out.println("shshs");
-    }
+       System.out.println("shshs");
+   }
 }
